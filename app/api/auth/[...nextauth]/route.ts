8c451153af285ddad3b5fe7a65d5793ec68f264c
sutarrohit <rohitsutar082@gmail.com>
2024-08-29 20:12:32 +0530

    feat: add new signin page

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,34 +1,35 @@
 import GoogleProvider from "next-auth/providers/google";
-import NextAuth from "next-auth"
+import NextAuth from "next-auth";
 import { prismaClient } from "@/app/lib/db";
 
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? "",
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ""
-        })
-    ],
-    secret: process.env.NEXTAUTH_SECRET ?? "secret",
-    callbacks: {
-        async signIn(params) {
-            if (!params.user.email) {
-                return false;
-            }
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET ?? "secret",
+  callbacks: {
+    async signIn(params) {
+      if (!params.user.email) {
+        return false;
+      }
 
-            try {
-                await prismaClient.user.create({
-                    data: {
-                        email: params.user.email,
-                        provider: "Google"
-                    } 
-                })
-             } catch(e) {
+      try {
+        await prismaClient.user.create({
+          data: {
+            email: params.user.email,
+            provider: "Google",
+          },
+        });
+      } catch (e) {}
+      return true;
+    },
+  },
+  pages: {
+    signIn: "/signin",
+  },
+});
 
-             }
-            return true;
-        }
-    }
-})
-
-export { handler as GET, handler as POST }
+export { handler as GET, handler as POST };
