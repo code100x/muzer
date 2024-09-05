4f41e14926d408d8825cc3f40c88d97842fddb42
Vishnu Kumar <vishnukumart96@gmail.com>
2024-09-05 19:53:44 +0530

    feat: add playNext POST endPoint

--- app/api/streams/next/route.ts
+++ app/api/streams/next/route.ts
@@ -1,6 +1,13 @@
 import { prismaClient } from "@/app/lib/db";
 import { getServerSession } from "next-auth";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+
+
+const playNextSchema = z.object({
+    streamId: z.string().uuid(),
+    txSig: z.string(),
+})
 
 export async function GET() {
     const session = await getServerSession();
@@ -60,4 +67,83 @@ export async function GET() {
     stream: mostUpvotedStream
    })
    
+}
+
+
+export async function POST(req:NextRequest){
+
+    const session = await getServerSession();
+    // TODO: You can get rid of the db call here 
+    const user = await prismaClient.user.findFirst({
+       where: {
+           email: session?.user?.email ?? ""
+       }
+   });
+
+   if (!user) {
+       return NextResponse.json({
+           message: "Unauthenticated"
+       }, {
+           status: 403
+       })
+   }
+
+    const data = await req.json();
+   
+    //TODO : CREATE A ZOD SCHEMA
+
+    const streamId = data.streamId as string;   
+    // const tx = data.tx as string;
+
+    //TODO: VERIFY THE TX SIG
+
+    /*
+        const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+        const txResponse = await connection.getTransaction(txnId,{ maxSupportedTransactionVersion: 0 });
+    */  
+
+
+    const nextStream = await prismaClient.stream.findUnique({
+        where:{
+            id:streamId,
+            userId:user.id,
+        }
+    })
+
+    if (!nextStream) {
+        return NextResponse.json({
+            message: "Enter Valid Stream Id !"
+        }, {
+            status: 404,
+        })
+    }
+    
+
+    await prismaClient.currentStream.delete({where:{userId:user.id}});
+
+    await Promise.all([prismaClient.currentStream.upsert({
+        where: {
+            streamId : nextStream.id,
+            userId:user.id
+        },
+        update: {
+            streamId : nextStream.id,
+        },
+        create: {
+            userId: user.id,
+            streamId : nextStream.id,
+        },
+    }),prismaClient.stream.update({
+        where: {
+            id : nextStream.id,
+        },
+        data: {
+            played: true,
+            playedTs: new Date()
+        }
+   })])
+
+   return NextResponse.json({
+    stream: nextStream
+   })
 }
