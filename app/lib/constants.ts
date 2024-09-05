b81cd667417c84005dfe176e4897fefb9c8fac62
Vishnu Kumar <vishnukumart96@gmail.com>
2024-09-05 21:54:12 +0530

    feat: add constants file

---
+++ app/lib/constants.ts
@@ -0,0 +1,3 @@
+export const DEFAULT_LAMPORT = 1000_00_00; // EQUALS TO 0.01 SOL
+export const PUBLIC_KEY_ADDRESS = process.env.NEXT_PUBLIC_ADDRESS || "";
+export const RPC_URI = process.env.NEXT_PUBLIC_RPC_URI || "";
