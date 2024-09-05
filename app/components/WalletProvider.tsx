a81185f6c4ac364434f808049df593c03110e8dd
Vishnu Kumar <vishnukumart96@gmail.com>
2024-09-05 21:55:42 +0530

    feat: use RPC_URI as the endpoint

--- app/components/WalletProvider.tsx
+++ app/components/WalletProvider.tsx
@@ -1,22 +1,16 @@
-import React, { FC, ReactNode, useMemo } from 'react';
+import React, { FC, ReactNode } from 'react';
 import { ConnectionProvider, WalletProvider } from '@solana/wallet-adapter-react';
-import { WalletAdapterNetwork } from '@solana/wallet-adapter-base';
 import {
     WalletModalProvider,
 } from '@solana/wallet-adapter-react-ui';
-import { clusterApiUrl } from '@solana/web3.js';
 
 // Default styles that can be overridden by your app
 import '@solana/wallet-adapter-react-ui/styles.css';
+import { RPC_URI } from '../lib/constants';
 
 export const Wallet: FC<{children:ReactNode}> = ({children}) => {
-    // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-    const network = WalletAdapterNetwork.Devnet;
-
-    // You can also provide a custom RPC endpoint.
-    // TODO: FIX THIS
-    const endpoint = useMemo(() => clusterApiUrl(network), [network]);
-
+    
+    const endpoint = RPC_URI;
 
     return (
         <ConnectionProvider endpoint={endpoint}>
