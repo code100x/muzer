ac44c2522bf89ff39fac448bb9e3c9d4739b3b02
Vishnu Kumar <vishnukumart96@gmail.com>
2024-09-05 20:33:29 +0530

    feat: add WalletProvider in Providers

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,11 +1,17 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
+import { Wallet } from "./components/WalletProvider";
+
 
 export function Providers({ children }: {
     children: React.ReactNode
 }) {
-    return <SessionProvider>
-        {children}
-    </SessionProvider>
+    return (
+        <SessionProvider>
+            <Wallet>
+                {children}
+            </Wallet>
+        </SessionProvider>
+    ) 
 }
