bacf99902f5656f6d15d06ec03e6079b8d99f5e7
Satyam Shubham <satyam.shubham@vectoscalar.com>
2024-09-14 01:15:14 +0530

    feat: next-themes setup

--- next-app/components/Appbar.tsx
+++ next-app/components/Appbar.tsx
@@ -1,32 +1,32 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import { ThemeSwitcher } from "./ThemeSwitcher";
 
 export function Appbar() {
   const session = useSession();
+  const isUserLoggedIn = session.data?.user;
+  const buttonTitle = isUserLoggedIn ? "Logout" : "Login";
+
+  const handleButtonClick = () => {
+    session.data?.user
+      ? signOut({ callbackUrl: "/" })
+      : signIn("google", { callbackUrl: "/dashboard" });
+  };
 
   return (
-    <div className="flex justify-between px-5 md:px-10 xl:px-20 py-4">
-      <div className="text-lg font-bold flex flex-col justify-center text-white">
+    <div className="flex justify-between px-5 py-4 md:px-10 xl:px-20">
+      <div className="flex flex-col justify-center text-lg font-bold text-white">
         Muzer
       </div>
-      <div>
-        {session.data?.user && (
-          <Button
-            className="bg-purple-600 text-white hover:bg-purple-700"
-            onClick={() => signOut({ callbackUrl: "/" })}
-          >
-            Logout
-          </Button>
-        )}
-        {!session.data?.user && (
-          <Button
-            className="bg-purple-600 text-white hover:bg-purple-700"
-            onClick={() => signIn("google", { callbackUrl: "/dashboard" })}
-          >
-            Signin
-          </Button>
-        )}
+      <div className="flex items-center gap-2">
+        <ThemeSwitcher />
+        <Button
+          className="bg-purple-600 text-white hover:bg-purple-700"
+          onClick={handleButtonClick}
+        >
+          {buttonTitle}
+        </Button>
       </div>
     </div>
   );
