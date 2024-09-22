25276593bdf81c3b6d928ec7e7e93840aea46f43
Venom <93427284+Mihir2423@users.noreply.github.com>
2024-09-23 00:57:27 +0530

    feat: Add Metadata (#113)
    * feat: add metdata and og image

--- next-app/app/layout.tsx
+++ next-app/app/layout.tsx
@@ -24,7 +24,24 @@ const globalToastOptions: ToastContainerProps = {
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Muzer",
+  metadataBase: new URL(process.env.NEXTAUTH_URL || "https://muzer.100xdevs.com/"),
+  keywords:
+    "music stream, fan interaction, live streaming, high-quality audio, curate music, Muzer",
+  title: "Muzer | Fan-Curated Live Music Streaming",
+  description:
+    "Live fan-curated music streaming. High-quality audio, real-time engagement.",
+  openGraph: {
+    type: "website",
+    locale: "en_IE",
+    url: `${process.env.NEXTAUTH_URL}/opengraph-image.png`,
+    images: "/opengraph-image.png",
+    siteName: "Infra",
+  },
+  icons: [
+    {
+      url: `${process.env.NEXTAUTH_URL}/favicon.ico`,
+    },
+  ],
 };
 
 export default function RootLayout({
