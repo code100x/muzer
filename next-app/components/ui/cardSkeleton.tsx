0abe414d2833e6330f896b49102514d96cc54b4c
geekyharsh05 <hp424805@gmail.com>
2024-09-28 18:23:38 +0530

    refactor: redesigned spaces card component.

--- next-app/components/ui/cardSkeleton.tsx
+++ next-app/components/ui/cardSkeleton.tsx
@@ -1,14 +1,16 @@
-import { Skeleton } from "@/components/ui/skeleton"
-import React from "react";
+import { Skeleton } from "@/components/ui/skeleton";
 
 export default function CardSkeleton() {
   return (
-    <div className="flex flex-col space-y-3">
-    <Skeleton className="h-[450px] w-[500px] rounded-3xl" />
-    <div className="space-x-5 flex justify-center">
-      <Skeleton className="h-[40px] w-[110px]" />
-      <Skeleton className="h-[40px] w-[152px]" />
+    <div className="flex flex-col space-y-4 p-4 md:p-6">
+      {/* Skeleton for the image */}
+      <Skeleton className="h-48 w-full rounded-t-2xl sm:h-64 md:h-72 lg:h-80 xl:h-96" />
+
+      {/* Skeleton for the buttons */}
+      <div className="flex flex-col space-y-2 p-4 sm:p-6 md:flex-row md:justify-between md:space-x-4 md:space-y-0">
+        <Skeleton className="h-10 w-full rounded-lg" />
+        <Skeleton className="h-10 w-full rounded-lg" />
+      </div>
     </div>
-  </div>
   );
 }
